97e720c257efcb130ee6dad6d2097a01b79aae51
Dominik Koller <mail@dominikkoller.com>
2024-09-21 00:08:45 +0200

    Revert "WIP collab"
    This reverts commit 101646c5dd4d05b045d0e7d0b185fcd82983be9a.

--- src/models/Document.ts
+++ src/models/Document.ts
@@ -7,16 +7,12 @@ export interface IDocument extends MDocument {
     title: string;
     content: any;
     owner: Schema.Types.ObjectId;
-    version: number;
-    steps: any[];
 }
 
 const DocumentSchema = new Schema<IDocument>({
     title: { type: String, required: true },
     content: { type: Schema.Types.Mixed, required: true },
     owner: { type: Schema.Types.ObjectId, ref: 'User', required: true},
-    version: { type: Number, default: 0 },
-    steps: { type: Schema.Types.Mixed, default: [] },
 });
 
 export default model<IDocument>('Document', DocumentSchema);
